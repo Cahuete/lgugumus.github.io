16af77fa7d22a12d02c26a563a93b25795df1f09
Cahuete <loic.gugumus@gmail.com>
2019-06-06 19:17:15 +0200

    stuiff stuff gnagna

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -1,14 +1,14 @@
-jQuery(document).ready(function() {
-    
-    jQuery("#S1").click(function() {
-        console.log("Jams");
-        jQuery('html,body').animate({
-            scrollTop: jQuery("#Jam").offset().top}, 'slow');
-    });
-    
-    jQuery("#S2").click(function() {
-        console.log("Pro");
-        jQuery('html,body').animate({
-            scrollTop: jQuery("#Pro").offset().top}, 'slow');
-    });
-});
+//jQuery(document).ready(function() {
+//    
+//    jQuery("#S1").click(function() {
+//        console.log("Jams");
+//        jQuery('html,body').animate({
+//            scrollTop: jQuery("#Jam").offset().top}, 'slow');
+//    });
+//    
+//    jQuery("#S2").click(function() {
+//        console.log("Pro");
+//        jQuery('html,body').animate({
+//            scrollTop: jQuery("#Pro").offset().top}, 'slow');
+//    });
+//});
